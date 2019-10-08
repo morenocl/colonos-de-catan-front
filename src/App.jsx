2acb1818dc2bb2cb1716e6cf153f754f9bdb8577
Juan P. Gomez <suanchi@gmail.com>
2019-10-08 00:55:35 -0300

    Se agregan Hooks al formulario de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
-import React, { Component } from "react";
-import NavBar from "./routing/Nav";
-import Routes from "./Routes";
+import React from "react";
+
+import Login from "./login/Login";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-/// "un-reduxed" component, this ideally should be a container, with its own .ducks file
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: null,
-      isAuthenticated: localStorage.getItem("token") ? true : false
-    };
-  }
-
-  // updates the main state
-  userHasAuthenticated = (authenticated, username, token) => {
-    this.setState({
-      isAuthenticated: authenticated,
-      username: username
-    });
-    localStorage.setItem("token", token);
-  };
-
-  // for our app, to be "logged out" is to remove the token
-  handleLogout = () => {
-    this.setState({
-      isAuthenticated: false,
-      username: ""
-    });
-    localStorage.removeItem("token");
-  };
-
-  // App just loads the navbar (that we could remove)
-  // and the Routes component, that defines the logic to move between pages
-  render() {
-    const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated
-    };
-    return (
-      <div className="App">
-        <NavBar
-          isAuthenticated={this.state.isAuthenticated}
-          handleLogout={this.handleLogout}
-        />
-        <Routes childProps={childProps} />
-      </div>
-    );
-  }
+function App() {
+  return <Login />;
 }
 
 export default App;
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,70 +1,37 @@
-import React, { Component } from "react";
-import LoginForm from "./LoginForm";
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import useLoginForm from "./LoginHook";
 
-import { login } from "../utils/httpService";
+export default function Login() {
+  const { inputs, handleInputChange, handleSubmit, validate } = useLoginForm();
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
-
-  componentDidMount() {
-    if (this.props.isAuthenticated) {
-      this.props.history.push("/");
-    }
-  }
-
-  validateForm(username, password) {
-    return username && username.length > 0 && (password && password.length > 0);
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
-
-  async handleSubmit(submitEvent) {
-    submitEvent.preventDefault();
-
-    try {
-      // await "holds" the execution until the async function completes
-      const loginResponse = await login({
-        username: this.state.username,
-        password: this.state.password
-      });
-      if (
-        loginResponse.user &&
-        loginResponse.user.username &&
-        loginResponse.token
-      ) {
-        this.props.userHasAuthenticated(
-          true,
-          loginResponse.user.username,
-          loginResponse.token
-        );
-        this.props.history.push("/");
-      }
-    } catch (error) {
-      alert(error);
-    }
-  }
-
-  render() {
-    return (
-      <LoginForm
-        username={this.state.username}
-        password={this.state.password}
-        handleChangeUsername={e => this.handleChange(e)}
-        handleChangePassword={e => this.handleChange(e)}
-        handleSubmit={e => this.handleSubmit(e)}
-        validate={this.validateForm}
-      />
-    );
-  }
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            value={inputs.username}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={inputs.password}
+            onChange={handleInputChange}
+            type="password"
+          />
+        </Form.Group>
+        <Button
+          block
+          disabled={!validate(inputs.username, inputs.password)}
+          type="submit"
+        >
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
