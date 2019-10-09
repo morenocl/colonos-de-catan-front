38e4d5ad5ff1399cbcca20639f864da81282b6e5
Juan P. Gomez <suanchi@gmail.com>
2019-10-09 03:29:59 -0300

    Removiendo el directorio rounting

--- src/routing/AppliedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Route } from "react-router-dom";
-
-// render the component C, but passing along the props
-export default ({ component: C, props: cProps, ...rest }) => (
-  <Route {...rest} render={props => <C {...props} {...cProps} />} />
-);
--- src/routing/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// for 404 purposes
-export default () => (
-  <div>
-    <h3>Sorry, page not found!</h3>
-  </div>
-);
