4a7b3969836192acf1e35f7a629a744e6cb67823
Juan P. Gomez <suanchi@gmail.com>
2019-10-13 12:25:08 -0300

    Pasando login a Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import LoginScreen from "./login/LoginScreen";
+import * as actions from './App.ducks';
+import Routes from './Routes';
+import NavBar from './nav/NavBar';
 
-function App() {
-  return <LoginScreen />;
-}
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default App;
+
+const mapStateToProps = (state) => ({
+  auth: state.App.auth,
+});
+
+const App = ({ auth, setAuth, setUser }) => {
+  const logout = () => {
+    setAuth(false);
+    setUser(null);
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <div className="App">
+      <NavBar
+        logout={logout}
+      />
+      <Routes auth={auth} />
+    </div>
+  );
+};
+
+
+export default connect(
+  mapStateToProps,
+  actions,
+)(App);
+
+
+App.propTypes = {
+  auth: PropTypes.bool.isRequired,
+  setAuth: PropTypes.func.isRequired,
+  setUser: PropTypes.func.isRequired,
+};
+
+mapStateToProps.propTypes = {
+  state: PropTypes.shape({
+    auth: PropTypes.bool,
+  }).isRequired,
+};
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,88 +1,69 @@
-import React, { useContext, useState } from "react";
-import { AuthContext } from "./LoginScreen";
-import { login } from "../utils/Api";
-import "./Login.css";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export const Login = () => {
-  const { dispatch } = useContext(AuthContext);
+import * as actions from '../App.ducks';
+import { login } from '../utils/Api';
+import LoginForm from './LoginForm';
+
+const Login = ({ setAuth, setUser }) => {
   const initialState = {
-    username: "",
-    password: "",
+    username: '',
+    password: '',
     isSubmitting: false,
-    errorMessage: null
+    errorMessage: '',
   };
 
   const [data, setData] = useState(initialState);
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     setData({
       ...data,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
-  const handleFormSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setData({
       ...data,
       isSubmitting: true,
-      errorMessage: null
+      errorMessage: '',
     });
 
-    const dispatchLogin = resJson => {
+    const dispatchLogin = (resJson) => {
       if (resJson.token) {
-        dispatch({
-          type: "LOGIN",
-          payload: { ...resJson, user: data.username }
-        });
+        setAuth(true);
+        setUser(data.username);
+        localStorage.setItem('token', JSON.stringify(resJson.token));
       }
     };
-    const setError = error => {
+    const setError = (error) => {
       setData({
         ...data,
         isSubmitting: false,
-        errorMessage: error.message
+        errorMessage: error.message,
       });
     };
+
     login(data.username, data.password, dispatchLogin, setError);
   };
 
   return (
-    <div className="login-container">
-      <div className="card">
-        <div className="container">
-          <form onSubmit={handleFormSubmit}>
-            <h1>Login</h1>
-            <label htmlFor="username">
-              Username
-              <input
-                type="text"
-                value={data.username}
-                onChange={handleInputChange}
-                name="username"
-                id="username"
-              />
-            </label>
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                value={data.password}
-                onChange={handleInputChange}
-                name="password"
-                id="password"
-              />
-            </label>
-            {data.errorMessage && (
-              <span className="form-error">{data.errorMessage}</span>
-            )}
-            <button disabled={data.isSubmitting}>
-              {data.isSubmitting ? "Loading..." : "Login"}
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <LoginForm
+      data={data}
+      handleSubmit={handleSubmit}
+      handleInputChange={handleInputChange}
+    />
   );
 };
-export default Login;
+
+export default connect(
+  null,
+  actions,
+)(Login);
+
+Login.propTypes = {
+  setAuth: PropTypes.func.isRequired,
+  setUser: PropTypes.func.isRequired,
+};
