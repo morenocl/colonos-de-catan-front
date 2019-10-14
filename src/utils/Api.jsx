5badd0cb464dcec997ac43cbe479e7127aa3ecaa
Christian L Moreno <christianluciano.m@gmail.com>
2019-10-14 17:58:31 -0300

    Agregado gameStatus. Retorna la informacion necesaria para que un jugador ejersa su turno.

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -75,6 +75,68 @@ export function playerHand(id, onSuccess, onFailure) {
   request(url, option, onSuccess, onFailure);
 }
 
+export function gameStatus(id, onSuccess, onFailure) {
+  const actions = `${path}/games/${id}/player/actions`;
+  const board = `${path}/games/${id}/board`;
+  const hand = `${path}/games/${id}/player`;
+  const info = `${path}/games/${id}`;
+  const endPoint = [actions, board, hand, info];
+
+  const option = {
+    method: 'GET',
+    headers: {
+      Authorization: `JWT ${localStorage.getItem('token')}`,
+    },
+  };
+
+  const getFromPlayers = (ps) => ({
+    settlements: ps.map((player) => ({
+      colour: player.colour,
+      settlements: player.settlements,
+    })),
+    cities: ps.map((player) => ({
+      colour: player.colour,
+      cities: player.cities,
+    })),
+    roads: ps.map((player) => ({
+      colour: player.colour,
+      roads: player.roads,
+    })),
+    players: ps.map((player) => ({
+      username: player.username,
+      colour: player.colour,
+      developmentCards: player.development_cards,
+      resourceCards: player.resources_cards,
+      lastGained: player.last_gained,
+    })),
+  });
+
+  return Promise.all(endPoint.map((e) => fetch(e, option)))
+    .then((rs) => rs.map((r) => r.ok ? r.json() : onFailure(Error(r.statusText))))
+
+    .then((rs) => {
+      const { settlements, cities, roads, players } = getFromPlayers(rs[3].players);
+
+      onSuccess({
+        actions: rs[0],
+        board: {
+          hexagons: rs[1],
+          robber: rs[3].robber,
+          settlements: settlements,
+          cities: cities,
+          roads: roads,
+        },
+        hand: rs[2],
+        info: {
+          players: players,
+          turn: rs[3].current_turn,
+          winner: rs[3].winner,      // Es opcional, fijarse como manejarlo
+        },
+      });
+    })
+    .catch(onFailure);
+}
+
 login.PropTypes = {
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
@@ -99,6 +161,12 @@ boardStatus.PropTypes = {
 };
 
 playerHand.PropTypes = {
+  id: PropTypes.number.isRequired,
+  onSuccess: PropTypes.func.isRequired,
+  onFailure: PropTypes.func.isRequired,
+};
+
+gameStatus.PropTypes = {
   id: PropTypes.number.isRequired,
   onSuccess: PropTypes.func.isRequired,
   onFailure: PropTypes.func.isRequired,
