981ec2d57d0be3dcb59edb9265e1b358d7253042
Christian L Moreno <christianluciano.m@gmail.com>
2019-10-19 20:23:31 -0300

    Escueta implementacion funcional del componente.

--- src/components/Rooms/Waiting.jsx
+++ src/components/Rooms/Waiting.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
-const Waiting = ({ room, onClick, }) => {
-    const { maxPlayers, name, owner, players } = room;
-  
+export const Waiting = ({ room, onClick, }) => {
+    const { max_players, name, owner, players } = room;
+
     return (
-      <>
+      <div>
         <Container>
-          {/* Datos */}
+          <Row>
+            <Col>
+              {name}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              Propietario: {owner}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              Maximo jugadores: {max_players}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              Jugadores: {players.join(", ")}
+            </Col>
+          </Row>
         </Container>
         <Button disabled={!onClick} onClick={onClick}>
           Start game
         </Button>
-      </>
+      </div>
     );
   };
+
+  export default Waiting;
