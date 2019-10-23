17379c49af9c26ba62c54cfed53b4c004a6f3652
Christian L Moreno <christianluciano.m@gmail.com>
2019-10-23 18:24:57 -0300

    Cambios para adaptar a la aplicacion.

--- src/containers/Rooms/Waiting.jsx
+++ src/containers/Rooms/Waiting.jsx
@@ -5,55 +5,52 @@ import PropTypes from 'prop-types';
 
 import Error from '../../components/Error';
 import WaitingScreen from '../../components/Rooms/Waiting';
-import { getRoom, startGame } from '../../utils/Api';
+import { getRoom, startGame } from '../../utils/Mock';
 import useInterval from '../../utils/UseInterval';
 
 
 const mapStateToProps = (state) => ({
-  user: state.Auth.username,
+  username: state.Auth.username,
 });
 
 
-export const Waiting = ({ user }) => {
+export const Waiting = ({ username }) => {
   const id = Number(useParams().id);
-  const [error, setError] = useState(false);
+  const [stage, setStage] = useState('empty');
   const [room, setRoom] = useState(null);
 
-  const onSuccess = (r) => { setRoom(r); };
-  const onFailure = () => { setError(true); };
+  const onSuccess = (r) => { setRoom(r); setStage('running'); };
+  const onFailure = () => { setStage('error'); };
 
-  const gameId = !!room && room.game_has_started ? room.game_id : undefined;
-  const iAmOwner = !!room && room.owner === user;
-
+  const gameId = !!room && room.game_has_started ? room.game_id : null;
+  const iAmOwner = !!room && room.owner === username;
   const onClick = () => {
-    startGame(id, undefined, onFailure);
+    startGame(id, onFailure);
   };
 
   const refresh = () => {
-    if (room === null || !room.game_has_started) {
+    if (!gameId) {
       getRoom(id, onSuccess, onFailure);
     }
   };
-  useInterval(refresh, 2000);
 
-  if (room === null) {
-    return <></>;
+  useInterval(refresh, 4000);
+
+  if (stage === 'empty') return <></>;
+
+  if (gameId) return (<Redirect to={`/game/${gameId}`} />);
+
+  if (stage === 'running') {
+    return (
+      <WaitingScreen room={room} onClick={iAmOwner ? onClick : null} />
+    );
   }
 
-  if (error) {
-    return (<Error />);
-  }
-  if (gameId) {
-    return (<Redirect to={`/game/${gameId}`} />);
-  }
-
-  return (
-    <WaitingScreen room={room} onClick={iAmOwner ? onClick : null} />
-  );
+  return (<Error />);
 };
 
 export default connect(mapStateToProps)(Waiting);
 
 Waiting.propTypes = {
-  user: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
 };
