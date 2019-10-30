776b823bdfd52a6650d96052ec63e0f18f7ce036
Juan P. Gomez <suanchi@gmail.com>
2019-10-30 20:49:16 -0300

    Se setea un jugador como clickeable

---
+++ src/containers/Info.ducks.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+const SET_CLICK = 'info/SET_CLICK';
+
+export const dispatchOnClick = (playerOnClick) => ({
+  type: SET_CLICK,
+  payload: { playerOnClick },
+});
+
+export const initialState = {
+  playerOnClick: null,
+};
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case SET_CLICK:
+      return { ...state, playerOnClick: action.payload.playerOnClick };
+
+    default: return state;
+  }
+};
+
+export default reducer;
+
+dispatchOnClick.propTypes = {
+  playerOnClick: PropTypes.func.isRequired,
+};
+
+reducer.propTypes = {
+  state: PropTypes.shape({
+    playerOnClick: PropTypes.func.isRequired,
+  }).isRequired,
+  action: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    payload: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- src/containers/Info.jsx
+++ src/containers/Info.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import GameInfo from '../components/GameInfo';
 import PlayerInfo from '../components/PlayerInfo';
 import { InfoType } from '../utils/ApiTypes';
-
+import { dispatchOnClick } from './Info.ducks';
 
 const mapStateToProps = (state) => ({
   players: state.Game.info.players,
@@ -13,18 +13,28 @@ const mapStateToProps = (state) => ({
   winner: state.Game.info.winner,
 });
 
+const mapDispatchToProps = ({
+  setOnClick: dispatchOnClick,
+});
+
+const playerOnClick = () => ((player) => () => (() => { console.log(`Clicked by:${player.username}`); }));
+
 export const Info = (props) => {
-  const { players, turn, winner } = props;
+  const {
+    players, turn, winner, setOnClick,
+  } = props;
+
+  setOnClick(playerOnClick());
 
   return (
     <div>
-      { players.map((player) => <PlayerInfo player={player} key={player.username} />) }
+      {players.map((player) => <PlayerInfo player={player} key={player.username} />)}
       <GameInfo turn={turn} winner={winner} />
     </div>
   );
 };
 
-export default connect(mapStateToProps)(Info);
+export default connect(mapStateToProps, mapDispatchToProps)(Info);
 
 mapStateToProps.propTypes = {
   state: InfoType,
@@ -34,6 +44,7 @@ Info.propTypes = {
   players: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   turn: PropTypes.shape({}).isRequired,
   winner: PropTypes.string,
+  setOnClick: PropTypes.func.isRequired,
 };
 
 Info.defaultProps = {
