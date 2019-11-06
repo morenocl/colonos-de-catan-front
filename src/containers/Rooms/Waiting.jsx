84ffca4969061a01a8b2a7558446d2304fa294c6
Juan P. Gomez <suanchi@gmail.com>
2019-11-06 14:34:01 -0300

    Se pasa el stage de Waiting a redux

--- src/containers/Rooms/Waiting.jsx
+++ src/containers/Rooms/Waiting.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -7,23 +7,25 @@ import Error from '../../components/Error';
 import WaitingScreen from '../../components/Rooms/Waiting';
 import { getRoom, startGame, cancelRoom } from '../../utils/Mock';
 import useInterval from '../../utils/UseInterval';
-import { dispatchRoom } from './Rooms.ducks';
+import { dispatchRoom, dispatchWaiting } from './Rooms.ducks';
 import { RoomType } from '../../utils/ApiTypes';
 
 
 const mapStateToProps = (state) => ({
   username: state.Auth.username,
   room: state.Rooms.room,
+  stage: state.Rooms.waitingStage,
 });
 
 const mapDispatchToProps = ({
   setRoom: dispatchRoom,
+  setStage: dispatchWaiting,
 });
 
-export const Waiting = ({ username, room, setRoom }) => {
+export const Waiting = ({
+  username, room, setRoom, setStage, stage,
+}) => {
   const { id } = useParams();
-
-  const [stage, setStage] = useState('empty');
 
   const onSuccess = (r) => {
     setRoom(r);
@@ -67,6 +69,8 @@ Waiting.propTypes = {
   username: PropTypes.string.isRequired,
   room: RoomType,
   setRoom: PropTypes.func.isRequired,
+  setStage: PropTypes.func.isRequired,
+  stage: PropTypes.string.isRequired,
 };
 
 Waiting.defaultProps = {
