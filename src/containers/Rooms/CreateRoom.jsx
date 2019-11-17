3a5a25b6368ef9800f80d74ae8a432d084fd5299
Juan P. Gomez <suanchi@gmail.com>
2019-11-17 01:05:59 -0300

    Se agregan modificaciones para mockear correctamente

--- src/containers/Rooms/CreateRoom.jsx
+++ src/containers/Rooms/CreateRoom.jsx
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import CreateScreen from '../../components/Rooms/CreateRoom';
-import { getBoards, createRoom } from '../../utils/Mock';
+import { getBoards, createRoom } from '../../utils/Api';
 import { dispatchRunning } from './Rooms.ducks';
 
 
@@ -19,7 +19,7 @@ const initialState = {
 
 };
 
-const mapDispatchToProps = ({
+export const mapDispatchToProps = ({
   setRunning: dispatchRunning,
 });
 
